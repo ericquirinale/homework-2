80371252fcf6235770c9f41e4ea04a38aed41ff9
Eric Quirinale <ericquirinale@gmail.com>
2020-10-04 12:28:18 -0400

    Add files via upload

--- hw2-autograder/hw2/ml.c
+++ hw2-autograder/hw2/ml.c
@@ -34,27 +34,49 @@ int main(int argc, char** argv){
     int attributesNum;
     int trainingNums;
     fscanf(fp, "%d\t%d\n", &attributesNum, &trainingNums);
-    printf("%d\n%d\n", attributesNum, trainingNums);
-    double matrix[attributesNum][trainingNums];
-    double priceVector[trainingNums];
-    double temp = 0.0;
-    for (size_t i = 0; i < attributesNum; i++) {
-      for (size_t j = 0; j < trainingNums; j++) {
-        printf("%s", "Num: ");
+    //printf("%d\n%d\n", attributesNum, trainingNums);
+    double **matrix = (double **)calloc(trainingNums, sizeof(double *));
+    for (size_t i=0; i<trainingNums; i++){
+      matrix[i] = (double *)calloc(attributesNum, sizeof(double));
+    }
+    double *priceVector = (double *)calloc(trainingNums, sizeof(double));
+    for (size_t i = 0; i < trainingNums; i++) {
+      for (size_t j = 0; j < attributesNum; j++) {
+        //printf("%s", "Num: ");
         fscanf(fp, "%lf%*c", &matrix[i][j]);
-        printf("%lf\n", matrix[i][j]);
+        //printf("%lf\t", matrix[i][j]);
       }
+      fscanf(fp, "%lf\n", &priceVector[i]);
+      /*printf("%s\t", "Price: ");
+      printf("%lf\n", priceVector[i]);
+      printf("%s\n", "New Line: ");*/
     }
   }
+  for (size_t i=0; i<trainingNums; i++){
+    free(matrix[i]);
+  }
+  for (size_t i=0; i<trainingNums; i++){
+    free(matrix[i]);
+  }
+  free(matrix);
+  free(priceVector);
 	return 0;
 }
 
 double** multiplyMatrix(double **matA, double **matB, int r1, int c1, int r2, int c2)
 {
     double** result=malloc(r1*sizeof(double*));
+    for (size_t i=0; i<r1; i++){
+      result[i] = (double *)calloc(r1, sizeof(double));
+    }
 
-    // your code goes here
-
+    for (size_t i = 0; i < r1-1; i++) {
+      for (size_t j = 0; j < c2-1; j++) {
+        for (size_t k = 0; k < r2; k++) {
+          result[i][j] += matA[i][k]*matB[k][j];
+        }
+      }
+    }
     return result;
 }
 
@@ -63,8 +85,15 @@ double** transposeMatrix(double** mat, int row, int col)
 {
 
 	double** matTran=malloc(col*sizeof(double*));
+  for (size_t i=0; i<row; i++){
+    matTran[i] = (double *)calloc(row, sizeof(double));
+  }
 
-    // your code goes here
+    for (size_t i = 0; i < row; i++) {
+      for (size_t j = 0; j < col; j++) {
+        matTran[j][i] = mat[i][j];
+      }
+    }
 
     return matTran;
 }
@@ -73,8 +102,23 @@ double** inverseMatrix(double **matA, int dimension)
 {
 
     double** matI=malloc(dimension*sizeof(double*));
+    for (size_t i=0; i<dimension; i++){
+      matI[i] = (double *)calloc(dimension, sizeof(double));
+    }
+    for (size_t i = 0; i < dimension; i++) {
+      for (size_t j = 0; j < dimension; j++) {
+        if(i==j){
+          matI[i][j] = 1.0;
+        }
+        else{
+          matI[i][j] = 0.0;
+        }
+      }
+    }
 
-    // your code goes here
+    for (size_t i = 0; i < dimension-1; i++) {
+      matI[i][i] =
+    }
 
 	return matI;
 }
